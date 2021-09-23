99856770fa16c27714db35000f40a08e69924735
Juan Esteban Feijoo Morales <juesfe95@gmail.com>
2021-09-22 21:56:57 -0500

    Crud completo

---
+++ ApiEFCore/Data/ApiEFCoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiEFCore.Models;
+
+namespace ApiEFCore.Data
+{
+    public class ApiEFCoreContext : DbContext
+    {
+        public ApiEFCoreContext (DbContextOptions<ApiEFCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiEFCore.Models.Book> Book { get; set; }
+    }
+}
